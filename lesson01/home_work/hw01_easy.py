247910098df9f1e6df198c3020e4156dc241d5b6
NieMandX <maragojeep@gmail.com>
2018-09-04 23:24:04 +0300

    lesson1_easy_tasks_1-2

--- lesson01/home_work/hw01_easy.py
+++ lesson01/home_work/hw01_easy.py
@@ -13,7 +13,7 @@ count = 0
 while count < len(str(random_number)):
 	print(str(random_number)[count])
 	count += 1
-	
+
 
 
 # Задача-2: Исходные значения двух переменных запросить у пользователя.
@@ -25,6 +25,25 @@ while count < len(str(random_number)):
 # print("a = ", b, "b = ", a) - это неправильное решение!
 
 
+# -----VARIABLE INTERCHANGE. MATH METHOD -------
+
+x = int(input("Enter X:"))
+y = int(input("Enter Y:"))
+
+x += y
+y = x - y
+x = x - y
+
+print("X and Y are interchanged by math method, X =", x," Y =", y)
+
+# -----VARIABLE INTERCHANGE. PYTHON METHOD WITH TEMPORARY TUPLE-------
+
+x, y = y, x
+
+print("X and Y are changed again by python method (temporary tuple), X =", x," Y =", y)
+
+
 # Задача-3: Запросите у пользователя его возраст.
 # Если ему есть 18 лет, выведите: "Доступ разрешен",
 # иначе "Извините, пользование данным ресурсом только с 18 лет"
+
