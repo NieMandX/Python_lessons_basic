dbff4bc0ada64160513df5bab692f456b047872a
NieMandX <maragojeep@gmail.com>
2018-09-20 15:35:52 +0300

    lesson 4 Easy and Normal

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -5,14 +5,60 @@
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
+from random import randint
+
+print('\n')
+print('{:*^60}\n'.format('Homework N4. Level: Easy. Task 1.'))
+
+numlist = [randint(0,50) for _ in range(20)]
+print (numlist)
+sqrlist = [i**2 for i in numlist]
+print(sqrlist)
+
+# ------------------------ONE LINE METHOD------------------------------
+
+# sqrlist = [i**2 for i in [randint(0,50) for _ in range(20)]]
+
+# ------------------------ONE LINE METHOD------------------------------
+
+input('Press Enter to continue...')
+
+
 
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
 
+print('\n')
+print('{:*^60}\n'.format('Homework N4. Level: Easy. Task 2.'))
+
+
+
+f1 = ['apple', 'orange', 'peach', 'kiwi', 'lemom', 'apricot', 'pineapple']
+f2 = ['mango', 'tangerine', 'avocado', 'papaya', 'kiwi', 'orange', 'peach', 'apple']
+print (f1)
+print (f2)
+
+f3 = [i for i in f1 if i in f2]
+print (f3)
+
+input('Press Enter to continue...')
 # Задание-3:
 # Дан список, заполненный произвольными числами.
 # Получить список из элементов исходного, удовлетворяющих следующим условиям:
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+print('\n')
+print('{:*^60}\n'.format('Homework N4. Level: Easy. Task 3.'))
+
+numlist = [randint(-0,20) for _ in range(30)]
+print (numlist)
+sortlist = [i for i in numlist if (i%3==0 and i%4 != 0 and i>0) ]
+print(sortlist)
+
+
+
+input('Press Enter to continue...')
+
--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -4,6 +4,12 @@
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
 
+import re
+from random import randint
+
+print('\n'*2)
+print('{:*^60}\n'.format('Homework N4. Level: Normal. Task 1. Method 1 (without re)'))
+
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,7 +26,34 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+word = ''
+newlist = []
+for letter in line:
+    if letter.islower():
+        word = word+letter
+        flag = 1
+    elif flag:
+        newlist.append(word)
+        word = ''
+        flag = 0
+    else:
+        pass
+else:
+    newlist.append(word)
 
+print (newlist)
+
+print()
+input('{:-^60}'.format('press Enter to continue'))
+
+print('\n'*2)
+print('{:*^60}\n'.format('Homework N4. Level: Normal. Task 1. Method 2 (with re)'))
+
+newlist_re = re.split(r'[A-Z]+',line)
+print (newlist_re)
+
+print()
+input('{:-^60}'.format('press Enter to continue'))
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
 # два символа в нижнем регистре, а справа - два символа в верхнем регистре.
@@ -28,6 +61,9 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+
+print('\n'*2)
+print('{:*^60}\n'.format('Homework N4. Level: Normal. Task 2. Method 1 (without re)'))
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +81,93 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+new_list2 =[]
+flag = 0
+l = 0
+b = 0
+for index in range(len(line_2)):
+    if line_2[index].islower():
+        if l >= 2 and b >=3:
+            new_list2.append(line_2[index-b:index-2])
+            l = 0
+            b = 0
+            flag = 1
+        elif flag == 0:
+            l = 0
+            b = 0
+            flag = 1
+        else:
+            pass
+        l = l + 1
+    else:
+        b = b + 1
+        flag = 0
+        
+print (new_list2)
+print()
+input('{:-^60}'.format('press Enter to continue'))
+
+print('\n'*2)
+print('{:*^60}\n'.format('Homework N4. Level: Normal. Task 2. Method 2 (with re)'))
+
+print ([i.group(1) for i in re.finditer(r'[a-z]{2}([A-Z]+)[A-Z]{2}',line_2)])
+
+print()
+input('{:-^60}'.format('press Enter to continue'))
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+print('\n'*2)
+print('{:*^60}\n'.format('Homework N4. Level: Normal. Task 2. Method 1 (without re)'))
+
+with open('random.txt', 'w', encoding = 'UTF-8') as f2:
+    f2.write(''.join(str (i) for i in [randint(0,9) for _ in range(2500)]))
+
+with open('random.txt', 'r', encoding = 'UTF-8') as f2:
+    bignum = (f2.read()) 
+    
+offset = 0
+start = 0
+maxstart = 0
+maxstarts = []
+maxoffset = 0
+flag = 0
+for index in range(len(bignum)-1):
+    if bignum [index] is bignum [index + 1]:
+        if flag == 0:
+            start = index
+            offset = 1
+            flag = 1
+        else:
+            flag = 1
+            offset += 1
+    else:
+        flag = 0
+        if offset > maxoffset:
+            maxstart = start
+            maxoffset = offset
+            maxstarts.clear()
+            
+        elif offset == maxoffset:
+            maxstarts.append(start)
+            offset = 0
+else:
+    if offset > maxoffset:
+        maxstart = start
+        maxoffset = offset
+        maxstarts.clear()
+            
+    elif offset == maxoffset:
+        maxstarts.append(start)
+       
+print (bignum)
+print ()
+if len(maxstarts):
+    for i in range(len(maxstarts)):
+        print ('Maximum similar sequence is: {} it start at {}'.format(bignum[maxstarts[i]:maxstarts[i]+maxoffset+1], maxstarts[i]+1))
+else:
+    print ('Maximum similar sequence is: {} it start at {}'.format(bignum[maxstart:maxstart+maxoffset+1], maxstart+1))
+   
